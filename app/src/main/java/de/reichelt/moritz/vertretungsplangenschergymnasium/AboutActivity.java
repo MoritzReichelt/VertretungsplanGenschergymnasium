49807c7d3d676036243c61750032120c3ba54189
StardOva <moritz.reichelt2001@gmail.com>
2018-07-30 18:23:45 +0200

    Updated Über diese App

--- app/src/main/java/de/reichelt/moritz/vertretungsplangenschergymnasium/AboutActivity.java
+++ app/src/main/java/de/reichelt/moritz/vertretungsplangenschergymnasium/AboutActivity.java
@@ -1,5 +1,7 @@
 package de.reichelt.moritz.vertretungsplangenschergymnasium;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -10,45 +12,29 @@ import java.text.DateFormat;
 import java.util.Date;
 
 public class AboutActivity extends AppCompatActivity {
-    String logPath;
-    String zipPath;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
 
-        logPath = "/storage/emulated/0/Android/data/" + getPackageName() + "/Logs";
-        zipPath = "storage/emulated/0/Android/data/" + getPackageName() + "/Zips";
-
-        File logDir = new File(logPath);
-        final File[] logFiles = logDir.listFiles();
-        final String[] filesString = new String[0];
-/*
-        if (logFiles.length > 0) {
-
-            for (int i = 0; i < logFiles.length; i++) {
-                filesString[i] = logFiles[i].toString();
+        Button seeLicense = findViewById(R.id.viewLicense);
+        seeLicense.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent viewLicense = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.gnu.org/licenses/gpl-3.0.txt"));
+                startActivity(viewLicense);
             }
+        });
 
-            Button crashButton = findViewById(R.id.buttonSendLog);
-            crashButton.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-
-                    File zipDir = new File(zipPath);
-                    if (!zipDir.exists()) {
-                        zipDir.mkdir();
-                    }
-                    String currentDateTime = DateFormat.getDateTimeInstance().format(new Date());
-                    currentDateTime = currentDateTime.replaceAll("\\s+", "_");
-                    currentDateTime = currentDateTime.replaceAll(":", "_");
-
-                    Zipper.zip(filesString, zipPath + currentDateTime);
-                }
-            });
-
-        }
-        */
+        Button viewCode = findViewById(R.id.viewCode);
+        viewCode.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent viewCode = new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/StardOva/VertretungsplanGenschergymnasium"));
+                startActivity(viewCode);
+            }
+        });
     }
 }
