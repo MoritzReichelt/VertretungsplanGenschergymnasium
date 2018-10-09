1732d5ba072dc18e74e7f4b33cc137f57c0ec17d
StardOva <moritz.reichelt2001@gmail.com>
2018-10-09 19:28:42 +0200

    Update auf Version 1.8 + Bugfixes

---
+++ app/src/main/java/de/reichelt/moritz/vertretungsplangenschergymnasium/Constants.java
@@ -0,0 +1,13 @@
+package de.reichelt.moritz.vertretungsplangenschergymnasium;
+
+final class Constants {
+    static final String firstStartKey = "firstStart";
+    static final String urlKey = "pref_url";
+    static final String usernameKey = "pref_username";
+    static final String passwordKey = "pref_password";
+    static final String stretchScreenKey = "pref_stretch";
+    static final String cacheKey = "pref_cache";
+    static final String notificationsKey = "pref_notifications_enabled";
+    static final String syncIntervalKey = "pref_sync_interval";
+    static final String networkTypeKey = "pref_network_type";
+}
---
+++ app/src/main/java/de/reichelt/moritz/vertretungsplangenschergymnasium/LoginActivity.java
@@ -0,0 +1,74 @@
+package de.reichelt.moritz.vertretungsplangenschergymnasium;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import static android.support.constraint.Constraints.TAG;
+
+
+public class LoginActivity extends AppCompatActivity {
+
+    private SharedPreferences mPreferences;
+    private SharedPreferences.Editor editor;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        /*
+        Spinner dropdown = findViewById(R.id.dropdownSpinner);
+
+        String[] items = {"Schüler","Lehrer"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, R.layout.custom_spinner_item, items);
+        dropdown.setAdapter(adapter);
+
+        */
+
+        Button loginConfirm = findViewById(R.id.loginConfirm);
+        final EditText loginUsername = findViewById(R.id.loginUsername);
+        final EditText loginPassword = findViewById(R.id.loginPassword);
+
+        InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        assert imm != null;
+        imm.hideSoftInputFromWindow(loginUsername.getWindowToken(),0);
+
+        mPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+
+        loginConfirm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String username = loginUsername.getText().toString();
+                String password = loginPassword.getText().toString();
+                if (username.length() == 0 || password.length() == 0){
+                    Toast.makeText(LoginActivity.this, "Ungültige Eingabe", Toast.LENGTH_SHORT).show();
+                } else {
+                    editor = mPreferences.edit();
+                    editor.putString(Constants.usernameKey, username);
+                    editor.putString(Constants.passwordKey, password);
+                    editor.putBoolean(Constants.firstStartKey, false);
+                    editor.apply();
+                    startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                }
+            }
+        });
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        finishAffinity();
+        System.exit(0);
+    }
+}
